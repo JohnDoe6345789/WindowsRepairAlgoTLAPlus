230dd0ef77db3385049c58fc41341bb7596aa631
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-11-27 20:32:40 +0000

    fix things

--- scripts/run-tlc.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [string]$Module = "STLRepairAlgo",
-    [string]$Config = "models/STLRepairAlgo.cfg"
-)
-
-.\scripts\bootstrap.ps1
-
-java -cp tools/tla2tools.jar tlc2.TLC -config $Config "spec/$Module.tla"
